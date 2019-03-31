958cfcfef00e65b951e473c0a4a2aa9d936db6b4
KB Total Recall <kanwar@gmail.com>
2019-03-31 13:38:18 -0700

    Heroku deployment with html routes

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -2,13 +2,12 @@ var path = require("path");
 
 // Survey Route
 
-function htmlRoutes(app) {
+module.exports  = function(app) {
     app.get("/", function(req, res){
-        res.sendFile(path.join(__dirname), "../../public/home.html");
+        res.sendFile(path.join(__dirname, "../../public/home.html"));
     });
 
     app.get("/survey", function(req, res){
-        res.sendFile(path.join(__dirname), "../../public/survey.html");
+        res.sendFile(path.join(__dirname , "../../public/survey.html"));
     });
 }
-module.exports = htmlRoutes(app);
--- server.js
+++ server.js
@@ -3,16 +3,18 @@ var express = require("express");
 
 var app = express();
 
-var PORT = process.env.PORT || 3000
+var PORT = process.env.PORT || 8000
 
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 
-require("./app/routes/apiRoutes")(app);
-require("./app/routes/htmlRoutes")(app);
+var htmlRoutes = require("./app/routes/htmlRoutes");
+var apiRoutes =  require("./app/routes/apiRoutes");
 
+htmlRoutes(app);
+//apiRoutes(app);
 
 app.listen(PORT, function(){
     console.log("The server is listening on port " + PORT);
